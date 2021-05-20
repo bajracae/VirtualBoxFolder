72980aa0e24d9502258fef40f953957d11302787
root <root@localhost.hsd1.or.comcast.net>
2021-05-20 01:15:23 +0000

    saving my files

---
+++ HW6/run.sh
@@ -0,0 +1,24 @@
+#!/bin/ash
+
+# IP Addresses (NOTE: CHANGE THESE TO YOUR HOSTS' IP ADDRESSES)
+HOST1=192.168.1.101
+HOST2=192.168.1.102
+
+# Generate an ssh key
+ssh-keygen -t rsa
+
+# NOTE: CHANGE THE IP ADDRESSES TO YOUR HOSTS' IP ADDRESSES
+ssh-copy-id -i ~/.ssh/id_rsa $HOST1
+ssh-copy-id -i ~/.ssh/id_rsa $HOST2
+
+# Run the playbook
+ansible-playbook webserver.yaml -i ./hosts.ini
+
+# Return the contents of "ansible-index.html"
+echo "== ${HOST1} content =="
+curl $HOST1
+
+echo
+
+echo "== ${HOST2} content =="
+curl $HOST2
